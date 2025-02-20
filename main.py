308a12ba4f7f9cad46e83eef0123d2de639e8649
kaandnn <120340223+kaandnn@users.noreply.github.com>
2025-02-20 15:31:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+#q1
+name = input('What is your name \n')
+print('Hello!', name)
+
+id = input(' What is your ID\n')
+print(' ID is ', id)
+#q2
+var1 = float(input)('What is number of var1 (var1):')
+var2 = float(input)('What is number of var2 (var2):')
+
+sum = var1+var2
+diff = var1-var2
+prod = var1*var2
+
+print(sum)
+print(diff)
+print(prod)
+
+#q4
+
+print("*\n**\n***\n**\n*")
+
+#q3
+
+name = input('What is the student name')
+
+labGrade =float(input)('Enter a lab grade(25%)')
+midtermGrade = float(input)('Enter a midterm grade (%35)')
+finalGrade = float(input)('Enter a final grade (%40)')
+
+lastScore = labGrade*(0.25)+midtermGrade*(0.35)+finalGrade*(0.40)
+
+print(' Name :', name)
+print('Lab grade :',labGrade)
+print('midter Grade :',midtermGrade)
+print('Final grade :',finalGrade)
+print('Last Score is :',lastScore)
